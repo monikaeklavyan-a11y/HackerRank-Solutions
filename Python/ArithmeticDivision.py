ad23b755a3e60bda28e07267d0603d520d7f34a3
Monika Devi <monikaeklavyan@gmail.com>
2026-06-29 07:59:02 +0530

    to implement division operator.
    Added problem statement and platform information as comments.

---
+++ Python/ArithmeticDivision.py
@@ -0,0 +1,8 @@
+#Problem : To implement Arithmetic Division Operator with python
+#Platform:HackerRank
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+    
